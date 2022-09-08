8a036a865f13ac33e2274a254557f346ec7cdaef
Gild04 <gildarooo4@gmail.com>
2022-09-08 14:21:07 +0530

    viewports

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).readyState(function(){
     $nav=$('.nav');
     $toggleCollapse=$('toggle-collapse');
 
-    /**on click functionalities */
+    /**On click functionalities */
     $toggleCollapse.click(function() {
         $nav.toggleClass('collapse');
     })
